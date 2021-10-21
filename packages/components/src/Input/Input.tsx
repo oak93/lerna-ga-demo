700b82b3240caadef61ad48a56cbf2cc478c213d
osman.akar <osman.akar@ciceksepeti.com>
2021-10-21 23:16:05 +0300

    fix: aria-label added to input component

--- packages/components/src/Input/Input.tsx
+++ packages/components/src/Input/Input.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
 
-const Input = (): ReactElement => <input />;
+const Input = (): ReactElement => <input aria-label="some label" />;
 
 export default Input;
