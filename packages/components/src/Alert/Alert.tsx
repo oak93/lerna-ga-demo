ec3cc8821dbc3588c33941628dded6aed1a93a48
osman.akar <osman.akar@ciceksepeti.com>
2021-10-26 13:48:31 +0300

    feat(alert): aria-label correction

--- packages/components/src/Alert/Alert.tsx
+++ packages/components/src/Alert/Alert.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
 
 const Alert = ({ children, color = 'red', backgroundColor = 'transparent' }: any): ReactElement => (
-  <div style={{ backgroundColor, color }} aria-label="alert">
+  <div style={{ backgroundColor, color }} aria-label="alert component">
     {children}
   </div>
 );
