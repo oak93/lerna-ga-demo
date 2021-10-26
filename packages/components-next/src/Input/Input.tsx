419e2010c6f4b21db59a98bbbacd300313637850
osman.akar <osman.akar@ciceksepeti.com>
2021-10-26 17:29:07 +0300

    fix: next input

--- packages/components-next/src/Input/Input.tsx
+++ packages/components-next/src/Input/Input.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
 
-const Input = (): ReactElement => <input aria-label="some label for input" />;
+const Input = (): ReactElement => <input aria-label="i am an input" />;
 
 export default Input;
