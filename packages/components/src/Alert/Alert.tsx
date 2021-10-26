2142f5c715c5253f23441a9f869570dc954be72b
osman.akar <osman.akar@ciceksepeti.com>
2021-10-26 13:36:07 +0300

    fix: alert component fix

--- packages/components/src/Alert/Alert.tsx
+++ packages/components/src/Alert/Alert.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
 
 const Alert = ({ children, color = 'red', backgroundColor = 'transparent' }: any): ReactElement => (
-  <div style={{ color, backgroundColor }} aria-label="alert">
+  <div style={{ backgroundColor, color }} aria-label="alert">
     {children}
   </div>
 );
