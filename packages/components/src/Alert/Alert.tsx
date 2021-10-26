cbbae522ff989e66241c8378f553afec1b0020df
osman.akar <osman.akar@ciceksepeti.com>
2021-10-26 13:33:35 +0300

    feat: new prop added to alert

--- packages/components/src/Alert/Alert.tsx
+++ packages/components/src/Alert/Alert.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement } from 'react';
 
-const Alert = ({ children, color = 'red' }: any): ReactElement => (
-  <div style={{ color }} aria-label="alert">
+const Alert = ({ children, color = 'red', backgroundColor = 'transparent' }: any): ReactElement => (
+  <div style={{ color, backgroundColor }} aria-label="alert">
     {children}
   </div>
 );
